f5d09e2cd0c11559f72433b3a1e9e6656bfdd0c4
shseo@digicaps.com <shseo@digicaps.com>
2018-09-28 11:38:39 +0900

    init

---
+++ src/hello/spring/MainApplication.java
@@ -0,0 +1,27 @@
+package hello.spring;
+
+import hello.spring.user.domain.User;
+import hello.spring.user.domain.UserDao;
+import java.sql.SQLException;
+
+public class MainApplication {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
